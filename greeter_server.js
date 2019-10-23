64852ceff1827ced4861d110524dd74bbfd5ea61
Md Riadul Islam <rdhawladar@gmail.com>
2019-10-23 17:16:27 +0600

    Initial commit

---
+++ greeter_server.js
@@ -0,0 +1,41 @@
+var PROTO_PATH = __dirname + '/helloworld.proto';
+
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+/**
+ * Implements the SayHello RPC method.
+ */
+function sayHello(call, callback) {
+  callback(null, {message: 'Hello ' + call.request.name + ' age: ' + call.request.age});
+}
+
+/**
+ * Implements the SayHello Again RPC method.
+ */
+function sayHelloAgain(call, callback) {
+  callback(null, {message: 'Hello Again ' + call.request.name});
+}
+
+/**
+ * Starts an RPC server that receives requests for the Greeter service at the
+ * sample server port
+ */
+function main() {
+  var server = new grpc.Server();
+  server.addService(hello_proto.Greeter.service, 
+    {sayHello: sayHello, sayHelloAgain: sayHelloAgain});
+  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+  server.start();
+}
+
+main();
